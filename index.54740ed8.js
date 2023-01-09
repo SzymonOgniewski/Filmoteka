4dab19e2d4fcd0a3ac652a865619ec07109767c1
SzymonOgniewski <szymonogniewski00@gmail.com>
2023-01-09 05:10:35 +0000

    Deploying to gh-pages from @ SzymonOgniewski/filmoteka-projekt@d35bc9dbad881c67fe1d24cf15ca829d595508b1 🚀

--- index.54740ed8.js
+++
@@ -1,2 +0,0 @@
-console.log((async function(){const t=await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=dd32b08009b8c26db83a645989914c74&page=${page}`);if(!t.ok)throw new Error(t.status);return await t.json()}));
-//# sourceMappingURL=index.54740ed8.js.map

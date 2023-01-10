aaa9010411f775e9f6d68edea93895d07b925326
qudlaty <106681377+Chriss350@users.noreply.github.com>
2023-01-10 19:14:56 +0100

    up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { showPopularMovies } from './partials/js/popularMovies';
+import { getWatched } from './partials/js/mylibrary';
+
+getWatched();
 
 window.addEventListener('load', e => {
   showPopularMovies();

85b92ace1ca81152fdd89dc04c4a10d94226d8ab
qudlaty <106681377+Chriss350@users.noreply.github.com>
2023-01-12 00:40:31 +0100

    up

--- src/partials/js/mylibrary.js
+++ src/partials/js/mylibrary.js
@@ -64,7 +64,15 @@ const addQueue = moveId => {
     return localStorage.setItem('queue', arr);
   }
 };
-const getQueue = () => {};
+const getQueue = () => {
+  let queueList = localStorage.getItem('queue');
+  if (queueList !== null) {
+    let arr = [];
+    arr = queueList.split(',');
+    return arr;
+  }
+  return;
+};
 
 const showWatched = (moviesArr, display) => {
   display.textContent = '';
@@ -75,6 +83,44 @@ const showWatched = (moviesArr, display) => {
 
   if (moviesArr === null) {
     console.log('No movies added to library');
+  }
+
+  moviesArr.forEach(async elm => {
+    let markup = '';
+    const data = await movieData(elm);
+    let genres = [];
+
+    data.genres.forEach(r => {
+      genres.push(r.name);
+    });
+    let genresStr = genres.slice(0, 2).toString().replace(',', ', ');
+
+    if (data.poster_path === null) {
+      data.poster_path =
+        'http://www2.kanazawa-it.ac.jp/moriken/Student/Noimg.jpg';
+    } else {
+      data.poster_path = `https://image.tmdb.org/t/p/w500//${data.poster_path}`;
+    }
+
+    markup += `<li class="move_gallery__item" data-movieid="${data.id}">
+    <img class="move_gallery__image" src="${data.poster_path}" width="265" height="398">
+     <div class="move_gallery__title">${data.title}</div>
+     <div class="move_gallery__genres">${genresStr} | ${data.release_date}</div>
+     </li>`;
+
+    display.insertAdjacentHTML('beforeend', markup);
+  });
+};
+
+const showQueue = (moviesArr, display) => {
+  display.textContent = '';
+
+  if (display === null) {
+    return console.log('error: no container to display');
+  }
+
+  if (moviesArr === null) {
+    console.log('No queue movies added to library');
   }
 
   moviesArr.forEach(async elm => {
@@ -98,4 +144,4 @@ const showWatched = (moviesArr, display) => {
   });
 };
 
-export { addWatched, getWatched, addQueue, getQueue, showWatched };
+export { addWatched, getWatched, addQueue, getQueue, showWatched, showQueue };

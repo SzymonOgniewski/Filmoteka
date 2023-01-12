f8a645a3a73748f6847839b765050be01f76168e
qudlaty <106681377+Chriss350@users.noreply.github.com>
2023-01-12 11:56:04 +0100

    myLibrary

--- src/partials/js/mylibrary.js
+++ src/partials/js/mylibrary.js
@@ -96,7 +96,7 @@ const removeQueue = (idsList, moveId) => {
   return localStorage.setItem('queue', filteredArr);
 };
 
-const showWatched = (moviesArr, display) => {
+const showMovies = (moviesArr, display) => {
   display.textContent = '';
 
   if (display === null) {
@@ -134,36 +134,4 @@ const showWatched = (moviesArr, display) => {
   });
 };
 
-const showQueue = (moviesArr, display) => {
-  display.textContent = '';
-
-  if (display === null) {
-    return console.log('error: no container to display');
-  }
-
-  if (moviesArr === null) {
-    console.log('No queue movies added to library');
-  }
-
-  moviesArr.forEach(async elm => {
-    let markup = '';
-    const data = await movieData(elm);
-
-    if (data.poster_path === null) {
-      data.poster_path =
-        'http://www2.kanazawa-it.ac.jp/moriken/Student/Noimg.jpg';
-    } else {
-      data.poster_path = `https://image.tmdb.org/t/p/w500//${data.poster_path}`;
-    }
-
-    markup += `<li class="move_gallery__item" data-movieid="${data.id}">
-    <img class="move_gallery__image" src="${data.poster_path}" width="265" height="398">
-     <div class="move_gallery__title">${data.title}</div>
-     <div class="move_gallery__genres">genres | ${data.release_date}</div>
-     </li>`;
-
-    display.insertAdjacentHTML('beforeend', markup);
-  });
-};
-
-export { addWatched, getWatched, addQueue, getQueue, showWatched, showQueue };
+export { addWatched, getWatched, addQueue, getQueue, showMovies };
